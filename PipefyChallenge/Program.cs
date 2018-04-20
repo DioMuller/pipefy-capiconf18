8e790dd8383eabb075e77d23eab7af83f382415e
Diogo Muller <diogo@radek.com.br>
2018-04-20 10:00:44 -0300

    Created print functions, fixed a few numbers.

--- PipefyChallenge/Program.cs
+++ PipefyChallenge/Program.cs
@@ -10,19 +10,21 @@ namespace PipefyChallenge
             {
                 Console.WriteLine("Por favor, digite um número entre 1 e 9. Digite 0 para finalizar.");
                 var value = Console.ReadKey().KeyChar;
+                Console.WriteLine(); // Space after Character.
 
                 if (value == '0') break;
 
                 if (value < '1' || value > '9')
                 {
                     Console.WriteLine("Valor Inválido!");
-                    Console.WriteLine("---");
+                    PrintSpacer();
                     continue;
                 }
 
                 byte val = (byte) ( value - '0');
+                PrintSpacer();
                 PrintNumber(value, val, val);
-                Console.WriteLine("---");
+                PrintSpacer();
             }
 
         }
@@ -36,22 +38,47 @@ namespace PipefyChallenge
             PrintLine(segments[1] ? character : ' ', size);
             // Top Columns
             PrintColumns(segments[0] ? character : ' ', segments[2] ? character : ' ', size);
+
+            // If there is no Separator, do not print the rest.
+            // This guarantees that 1 and 7 won't have repeat columns.
+            if (!segments[3]) return;
+
             // Middle
             PrintLine(segments[3] ? character : ' ', size);
             // Bottom Columns
             PrintColumns(segments[4] ? character : ' ', segments[5] ? character : ' ', size);
+
             //Bottom
             PrintLine(segments[6] ? character : ' ', size);
         }
 
         private static void PrintLine(char character, int size)
         {
+            for (int i = 0; i < size; i++)
+                Console.Write(character);
 
+            Console.Write("\n");
         }
 
         private static void PrintColumns(char startChar, char endChar, int size)
         {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(startChar);
 
+                for (int j = 0; j < size - 2; j++)
+                    Console.Write(' ');
+
+                Console.Write(endChar);
+                Console.Write("\n");
+            }
+        }
+
+        private static void PrintSpacer()
+        {
+            Console.WriteLine();
+            Console.WriteLine("---");
+            Console.WriteLine();
         }
 
         private static bool[] GetSegment(byte value)
@@ -61,17 +88,17 @@ namespace PipefyChallenge
                 case 1:
                     return new bool[] { false, true, false, false, false, false, false };
                 case 2:
-                    return new bool[] { false, true, true, true, true, true, false };
+                    return new bool[] { false, true, true, true, true, false, true };
                 case 3:
                     return new bool[] { false, true, true, true, false, true, true };
                 case 4:
-                    return new bool[] { true, false, true, true, false, false, true };
+                    return new bool[] { true, false, true, true, false, true, false };
                 case 5:
                     return new bool[] { true, true, false, true, false, true, true };
                 case 6:
                     return new bool[] { true, true, false, true, true, true, true };
                 case 7:
-                    return new bool[] { false, true, true, true, false, false, true };
+                    return new bool[] { false, true, true, false, false, true, false };
                 case 8:
                     return new bool[] { true, true, true, true, true, true, true };
                 case 9:
